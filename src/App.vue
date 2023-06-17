4c3d8df10d87c7aae6007514a6a35f64d0b7a7dd
idangal8076 <idan7@post.bgu.ac.il>
2023-06-17 13:58:34 +0300

    '
    '

---
+++ src/App.vue
@@ -0,0 +1,104 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link :to="{ name: 'main' }">Vue Recipes</router-link>|
+      <router-link :to="{ name: 'search' }">Search</router-link>|
+      {{ !$root.store.username }}
+      <span v-if="!$root.store.username">
+        Guest:
+        <router-link :to="{ name: 'register' }">Register</router-link>|
+        <router-link :to="{ name: 'login' }">Login</router-link>|
+      </span>
+      <span v-else>
+        {{ $root.store.username }}: <button @click="Logout">Logout</button>|
+      </span>
+    </div>
+    <router-view />
+  </div>
+</template>
+
+
+
+<!-- <template>
+  
+  <div id="app">
+    <b-navbar toggleable="lg" type="dark" variant="info">
+      <b-navbar-brand to="/">Vue Recipes</b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item to="/">Home</b-nav-item>
+          <b-nav-item to="/search">Search</b-nav-item>
+        </b-navbar-nav>
+
+</template>      
+        <b-navbar-nav class="ml-auto">
+          <b-nav-form>
+            <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
+            <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
+          </b-nav-form>
+
+          <b-nav-item-dropdown text="Lang" right>
+            <b-dropdown-item href="#">EN</b-dropdown-item>
+            <b-dropdown-item href="#">ES</b-dropdown-item>
+            <b-dropdown-item href="#">RU</b-dropdown-item>
+            <b-dropdown-item href="#">FA</b-dropdown-item>
+          </b-nav-item-dropdown>
+
+          <b-nav-item-dropdown right>
+           
+            <template #button-content>
+              <em>User</em>
+            </template>
+            <b-dropdown-item href="#">Profile</b-dropdown-item>
+            <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+          </b-nav-item-dropdown>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+    <router-view />
+  </div>
+</template> -->
+
+<script>
+export default {
+  name: "App",
+  methods: {
+    Logout() {
+      this.$root.store.logout();
+      this.$root.toast("Logout", "User logged out successfully", "success");
+
+      this.$router.push("/").catch(() => {
+        this.$forceUpdate();
+      });
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+@import "@/scss/form-style.scss";
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  min-height: 100vh;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
---
+++ src/pages/MainPage.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="container">
+    <h1 class="title">Main Page</h1>
+    <RecipePreviewList title="Randome Recipes" class="RandomRecipes center" />
+    <router-link v-if="!$root.store.username" to="/login" tag="button">You need to Login to vue this</router-link>
+    {{ !$root.store.username }}
+    <RecipePreviewList
+      title="Last Viewed Recipes"
+      :class="{
+        RandomRecipes: true,
+        blur: !$root.store.username,
+        center: true
+      }"
+      disabled
+    ></RecipePreviewList>
+    <!-- <div
+      style="position: absolute;top: 70%;left: 50%;transform: translate(-50%, -50%);"
+    >
+      Centeredasdasdad
+    </div> -->
+  </div>
+</template>
+
+<script>
+// import { getRandomValues } from "crypto";
+import RecipePreviewList from "../components/RecipePreviewList";
+export default {
+  name: "MainPage",
+  components: {
+    RecipePreviewList,
+  },
+  // mounted(){
+  //   this.getRandomValues();
+  // },
+
+  // methods: {
+  //   async getRandomValues() {
+  //     //"https://api.spoonacular.com/recipes/random"
+  //     console.log("ll")
+  //     try {
+  //       console.log("hellooooooooooooo")
+  //       const response = await this.axios.get("http://localhost:3000/recipes/random")
+  //       console.log(response)
+  //       const recipes = response.data;
+  //       this.recipes = [];
+  //       this.recipes.push(...recipes)
+  //       console.log(this.recipes);
+  //     } catch (error) {
+  //       console.log(error);
+  //     }
+     
+  //   },
+  // },
+  
+};
+</script>
+
+
+
+<style lang="scss" scoped>
+.RandomRecipes {
+  margin: 10px 0 10px;
+}
+.blur {
+  -webkit-filter: blur(5px); /* Safari 6.0 - 9.0 */
+  filter: blur(2px);
+}
+::v-deep .blur .recipe-preview {
+  pointer-events: none;
+  cursor: default;
+}
+</style>
